e187de671f9f8fded093f73ab753a89cc7d57dd4
A00432835 <46534961+A00432835@users.noreply.github.com>
2019-04-08 14:35:20 -0300

    added validations in payment page

--- Lunch Box/App_Start/BundleConfig.cs
+++ Lunch Box/App_Start/BundleConfig.cs
@@ -22,6 +22,9 @@ namespace Lunch_Box
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                      "~/Scripts/validation.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
---
+++ Lunch Box/Scripts/validation.js
@@ -0,0 +1,53 @@
+﻿
+
+
+
+
+
+
+
+
+function getElems() {
+    var NameOnCard = document.getElementById("nameOnCard").value;
+    var cardType = document.getElementById("cardType").value;
+    var cardNumber = document.getElementById("cardNumber").value;
+    var expiryDate = document.getElementById("expiryDate").value;
+    var cvv = document.getElementById("cvv").value;
+
+    if (!(NameOnCard) || !(cardType) || !(cardNumber) || !(expiryDate) || !(cvv)) {
+        document.getElementById('error').innerHTML = "Please Fill all the values";
+        document.getElementById('error').style.color = "red";
+        return false;
+    }
+    else {
+        checkValues(NameOnCard, cardType, cardNumber, expiryDate, cvv);
+    }
+}
+
+
+function checkValues(NameOnCard, cardType, cardNumber, expiryDate, cvv) {
+    var regexAlpha = "[a-zA-Z]+";
+    var regexNumber = "[0-9]+";	
+    var firstDig = cardNumber.substr(0,1)
+    var twoDig = cardNumber.substr(0, 2)
+    if (!NameOnCard.match(regexAlpha)) {
+        return false;
+    }
+
+    if (cardNumber.length > 13 && cardNumber.length <= 16) {
+        if (!cardNumber.match(regexNumber)) {
+            return false;
+        }
+    }
+    if ((!expiryDate.match(regexNumber)) && expiryDate.length != 4) {
+        return false;
+    }
+    else {
+        var month = expiryDate.substr(0, 2)
+        if (!(month > 0) && month <= 12) {
+
+        }
+    }
+    //else if ()
+
+}
